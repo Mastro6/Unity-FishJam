e08331b8224d8b84802fe717826d163bb3885b51
JulianCummaudo <julian.cummaudo@gmail.com>
2024-04-02 16:30:53 +0200

    Fish movement in progress

--- Fish-jam/Assets/Movement.cs
+++ Fish-jam/Assets/Movement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -13,9 +15,13 @@ public class Movement : MonoBehaviour
     private float horizontal;
     private float vertical;
 
+    private float z;
     private float speed;
 
-    private bool lookright;
+    private bool lookRight;
+
+    private bool lookUp;
+    Vector2 notZero;
 
     // Start is called before the first frame update
     void Start()
@@ -33,15 +39,31 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
+
         vel = new Vector2(horizontal * speed, vertical * speed);
         rb.velocity = vel;
-        float z = Mathf.Acos(vel.y / vel.x);
 
-        print(z);
 
+        lookRight = vel.x==0?lookRight:vel.x>0;
+        lookUp = vel.y==0?lookUp:vel.y>0;
+
+        Vector3 ls = transform.localScale;
+
+        ls = new Vector3(lookRight?1:-1,ls.y,ls.z);
+        
+        transform.localScale = ls;
+
+
+        if(vel.x==0 && vel.y==0){
+            z = 0;
+        }else if(vel.x==0){
+            z = lookUp?Mathf.PI/2:-Mathf.PI/2;
+        }else{
+            z = Mathf.Atan(vel.y / vel.x);
+        }
 
         transform.eulerAngles = new Vector3(0, 0, z * 180 / Mathf.PI);
-
 
 
     }
