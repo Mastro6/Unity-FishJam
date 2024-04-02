a8154e2026bd64b171e4cc90e1a2fa085e502fd7
Mastro6 <alessandromastroianni6@gmail.com>
2024-04-02 18:49:25 +0200

    Changed slightly movement

--- Fish-jam/Assets/Scripts/Movement.cs
+++ Fish-jam/Assets/Scripts/Movement.cs
@@ -7,27 +7,26 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
 
-    Vector2 vel;
+    public Vector2 vel;
 
 
     private float horizontal;
     private float vertical;
 
     private float z;
-    private float speed;
+    public float speed;
 
     private bool lookRight;
 
     private bool lookUp;
-    Vector2 lsnz;
+    public Vector2 lsnz;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        speed = 10f;
     }
 
     // Update is called once per frame
@@ -35,18 +34,23 @@ public class Movement : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        vel = new Vector2(horizontal * speed, vertical * speed);
+        if (vel.magnitude > 10) vel = vel.normalized * 10;
+
+        Vector2 vector2raw = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (vector2raw.magnitude != 0)
+        {
+            lsnz = vel;
+        }
     }
 
     private void FixedUpdate()
     {
         
-
-        vel = new Vector2(horizontal * speed, vertical * speed);
         rb.velocity = vel;
 
-        if(vel.magnitude != 0) lsnz = vel;
-
-
+        
 
         lookRight = lsnz.x==0?lookRight: lsnz.x>0;
         lookUp = lsnz.y==0?lookUp: lsnz.y>0;
@@ -58,9 +62,7 @@ public class Movement : MonoBehaviour
         transform.localScale = ls;
 
 
-        if(lsnz.x==0 && lsnz.y==0){
-            z = 0;
-        }else if(lsnz.x==0){
+        if(lsnz.x==0){
             z = lookUp?Mathf.PI/2:-Mathf.PI/2;
             z = lookRight?z:-z;
         }else{
